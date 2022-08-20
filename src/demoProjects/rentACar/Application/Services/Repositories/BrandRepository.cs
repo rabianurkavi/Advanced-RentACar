50b9b460336b3bdf2fcd16891e872496bd063694
Rabia Nur Kavi <77841148+rabianur412@users.noreply.github.com>
2022-08-20 17:43:59 +0300

    creation of architecture

---
+++ src/demoProjects/rentACar/Application/Services/Repositories/BrandRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+using Domain.Entities;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services.Repositories
+{
+    public class BrandRepository : EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        //BURASI BASEDBCONTEXTE GÖRE ÇALIŞACAK
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ src/demoProjects/rentACar/Domain/Entities/Brand.cs
@@ -0,0 +1,27 @@
+﻿using Core.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core.CustomTypeProviders;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Brand:Entity
+    {
+       
+        public string BrandName { get; set; }
+        public Brand()
+        {
+
+        }
+
+        public Brand(int id,string brandName):this()
+        {
+            Id = id;
+            BrandName = brandName;
+        }
+    }
+}
